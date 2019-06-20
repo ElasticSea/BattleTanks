7f3e8f2dd7fd2f00e5857b0eb2efaf93af552263
ElasticSea <info@elasticsea.com>
2019-06-20 12:59:18 +0200

    Simple scene, tank responds to user input.

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        [Header("Parameters")]
+        [SerializeField] private float moveSpeed = 10;
+        
+        private void Update()
+        {
+            var input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.position += input * moveSpeed * Time.deltaTime;
+        }
+    }
+}
