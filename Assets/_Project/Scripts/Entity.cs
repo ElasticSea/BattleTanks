879028e23d6d1dcc53e1c10837741dc8cfcd9b50
ElasticSea <info@elasticsea.com>
2019-06-20 14:22:58 +0200

    Tank explodes on death.

--- Assets/_Project/Scripts/Entity.cs
+++ Assets/_Project/Scripts/Entity.cs
@@ -4,8 +4,25 @@ namespace _Project.Scripts
 {
     public class Entity : MonoBehaviour
     {
+        [Header("Parameters")]
         [SerializeField] private Team team;
 
+        [Header("Refenreces")]
+        [SerializeField] private Health health;
+        [SerializeField] private GameObject explosionPrefab;
+        
         public Team Team => team;
+
+        private void Awake()
+        {
+            health.OnDeath += () =>
+            {
+                var explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                explosion.GetComponent<ParticleSystem>().Play();
+                explosion.GetComponent<AudioSource>().Play();
+                
+                Destroy(gameObject);
+            };
+        }
     }
 }
