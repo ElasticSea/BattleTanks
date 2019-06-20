f3dd7417ee31238be1fa2c969743e86778bb54ad
ElasticSea <info@elasticsea.com>
2019-06-20 13:13:40 +0200

    Tank moves forward and rotates.

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -6,6 +6,7 @@ namespace _Project.Scripts
     {
         [Header("Parameters")]
         [SerializeField] private float moveSpeed = 10;
+        [SerializeField] private float rotateSpeed = 90;
 
         [Header("References")]
         [SerializeField] private Rigidbody rigidbody;
@@ -15,6 +16,7 @@ namespace _Project.Scripts
             var keyboardInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             
             rigidbody.MovePosition(transform.position + transform.forward * keyboardInput.y * moveSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, rotateSpeed * keyboardInput.x * Time.deltaTime, Space.World);
         }
     }
 }
