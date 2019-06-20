6db1fd06292784b0ffc235b0d0a966fd540c73da
ElasticSea <info@elasticsea.com>
2019-06-20 13:21:31 +0200

    Move tank turret separately to the body.

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -7,9 +7,11 @@ namespace _Project.Scripts
         [Header("Parameters")]
         [SerializeField] private float moveSpeed = 10;
         [SerializeField] private float rotateSpeed = 90;
+        [SerializeField] private Vector2 turretSpeed = new Vector2(3, 2);
 
         [Header("References")]
         [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private Transform turret;
         
         private void FixedUpdate()
         {
@@ -17,6 +19,14 @@ namespace _Project.Scripts
             
             rigidbody.MovePosition(transform.position + transform.forward * keyboardInput.y * moveSpeed * Time.deltaTime);
             transform.Rotate(Vector3.up, rotateSpeed * keyboardInput.x * Time.deltaTime, Space.World);
+            
+        }
+        
+        private void Update()
+        {
+            var mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            turret.Rotate(Vector3.up, mouseInput.x * turretSpeed.x * Time.deltaTime, Space.World);
+            turret.Rotate(Vector3.left, mouseInput.y * turretSpeed.y * Time.deltaTime, Space.Self);
         }
     }
 }
